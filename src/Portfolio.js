aebc94ae48704102ee78ca22e4c811f19be12838
Mathias007 <mathias.najlepszy@gmail.com>
2021-05-02 01:19:36 +0200

    clean index.js, rename App to Portfolio, extend index.html (meta)

---
+++ src/Portfolio.js
@@ -0,0 +1,15 @@
+import Header from "./components/general/Header";
+import Main from "./components/general/Main";
+import Footer from "./components/general/Footer";
+
+function Portfolio() {
+    return (
+        <div className="portfolio">
+            <Header />
+            <Main />
+            <Footer />
+        </div>
+    );
+}
+
+export default Portfolio;
