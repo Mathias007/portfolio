3f357a7621bdfe9de1ad691252ade3ac39143f0d
Mathias007 <mathias.najlepszy@gmail.com>
2021-05-01 23:02:28 +0200

    Create AboutContentBox, implement BlockHeading in About.js

---
+++ src/components/blocks/boxes/AboutContentBox.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function AboutContentBox(props) {
+    return (
+        <div className="about__content-box">
+            <h3 className="about__subheader">
+                Jestem Frontend Web Developerem.
+            </h3>
+            <p className="about__text">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit
+                voluptatum illum sed beatae. Magni commodi dicta quia quis alias
+                reiciendis ea unde beatae sapiente enim labore, qui sint in
+                consectetur? Perspiciatis, odio expedita vitae perferendis
+                sapiente magni dolore blanditiis inventore officiis eum veniam
+                eveniet. Quod inventore eveniet voluptates voluptas. Dolore
+                velit nisi, minima explicabo ipsum a laborum praesentium at
+                vero! <br />
+                <br />
+                Earum error aut recusandae? Nobis laboriosam optio deleniti
+                architecto veniam, quam ipsam sed ex est odit, odio maiores quas
+                dolorum explicabo vero officia voluptate reiciendis enim a
+                eveniet, corporis reprehenderit.
+            </p>
+        </div>
+    );
+}
