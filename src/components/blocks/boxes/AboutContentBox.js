c727942ec3aae46fe7b4d1be9565ca07028ed7d1
Mathias007 <mathias.najlepszy@gmail.com>
2021-05-03 01:29:43 +0200

    extend and correct About component & style.

--- src/components/blocks/boxes/AboutContentBox.js
+++ src/components/blocks/boxes/AboutContentBox.js
@@ -52,9 +52,9 @@ export default function AboutContentBox(props) {
                             />
                         ) : null}
                     </div>
-                    <p className="about__text">
+                    <div className="about__text">
                         {contentText ? ReactHtmlParser(contentText) : null}
-                    </p>
+                    </div>
                 </div>
             </section>
         );
