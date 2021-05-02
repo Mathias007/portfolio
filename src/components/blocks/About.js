8e185bc1748b95491f8c153a42f78b276ca11f65
Mathias007 <mathias.najlepszy@gmail.com>
2021-05-03 01:05:35 +0200

    generate AboutContentBoxes using aboutData.

--- src/components/blocks/About.js
+++ src/components/blocks/About.js
@@ -3,33 +3,37 @@ import React from "react";
 import BlockHeading from "./heading/BlockHeading";
 import AboutContentBox from "./boxes/AboutContentBox";
 
+import { aboutData } from "../../data/aboutData";
+
 export default function About() {
     return (
         <article className="portfolio__block about" id="about">
             <BlockHeading
                 headingBlockClass="about__heading"
-                headingTitle="O mnie"
+                headingTitle="O mnie, czyli historia pewnego pasjonata"
             />
 
             <div className="about__content block-content">
-                <AboutContentBox
-                    contentSubheader="Jestem Frontend Web Developerem."
-                    contentText="Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Odit voluptatum illum sed beatae. Magni commodi dicta quia
-                    quis alias reiciendis ea unde beatae sapiente enim labore,
-                    qui sint in consectetur? Perspiciatis, odio expedita vitae
-                    perferendis sapiente magni dolore blanditiis inventore
-                    officiis eum veniam eveniet. Quod inventore eveniet
-                    voluptates voluptas. Dolore velit nisi, minima explicabo
-                    ipsum a laborum praesentium at vero! <br />
-                    <br />
-                    Earum error aut recusandae? Nobis laboriosam optio deleniti
-                    architecto veniam, quam ipsam sed ex est odit, odio maiores
-                    quas dolorum explicabo vero officia voluptate reiciendis
-                    enim a eveniet, corporis reprehenderit"
-                    imagePath="images/img1.jpg"
-                    imageDescription="img1"
-                />
+                {aboutData.map((element) => {
+                    const {
+                        id,
+                        subheader,
+                        paragraph,
+                        imagePath,
+                        imageDescription,
+                    } = element;
+
+                    return (
+                        <AboutContentBox
+                            id={id}
+                            key={id}
+                            contentSubheader={subheader}
+                            contentText={paragraph}
+                            imagePath={imagePath}
+                            imageDescription={imageDescription}
+                        />
+                    );
+                })}
             </div>
         </article>
     );
