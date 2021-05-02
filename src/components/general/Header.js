0e7cf55746253a80c6901460f8f70cb7135b03cf
Mathias007 <mathias.najlepszy@gmail.com>
2021-05-02 13:49:02 +0200

    handle toggle "sticky" and "active" menu classes using hooks; correct styles.

--- src/components/general/Header.js
+++ src/components/general/Header.js
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import Navigation from "./Navigation";
 
 export default function Header() {
+    const [isSticky, setSticky] = useState(false);
+    const [isActive, setActive] = useState(false);
+
+    const toggleStickyHeader = () => {
+        if (window.scrollY > 0) setSticky(!isSticky);
+        else setSticky(isSticky);
+    };
+
+    useEffect(() => {
+        function watchScroll() {
+            window.addEventListener("scroll", toggleStickyHeader);
+        }
+        watchScroll();
+        return () => {
+            window.removeEventListener("scroll", toggleStickyHeader);
+        };
+    }, []);
+
+    const toggleMenu = () => {
+        setActive(!isActive);
+    };
+
     return (
-        <header className="site-header">
+        <header className={`site-header ${isSticky ? "sticky" : ""}`}>
             <a href="#" className="site-header__logo">
                 Portfolio
             </a>
-            <div className="site-header__toggle toggle"></div>
-            <Navigation />
+            <div
+                className={`site-header__toggle ${isActive ? "active" : ""}`}
+                onClick={toggleMenu}
+            ></div>
+            <Navigation isActive={isActive} />
         </header>
     );
 }
--- src/components/general/Navigation.js
+++ src/components/general/Navigation.js
@@ -2,10 +2,12 @@ import React from "react";
 
 import NavElement from "./navigation/NavElement";
 
-export default function Navigation() {
+export default function Navigation(props) {
+    const { isActive } = props;
+
     return (
         <nav className="site-header__menu navigation">
-            <ul className="navigation__list">
+            <ul className={`navigation__list ${isActive ? "active" : ""}`}>
                 <NavElement path="#home" title="Start" />
                 <NavElement path="#about" title="O mnie" />
                 <NavElement path="#services" title="Usługi" />
