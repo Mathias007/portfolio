5987900bbafe00a736619bfb68643b431a31ceb7
Mathias007 <mathias.najlepszy@gmail.com>
2021-05-02 17:28:08 +0200

    refactor About and AboutContentBox (reusability, extendibility), implement Image.js

--- src/components/blocks/About.js
+++ src/components/blocks/About.js
@@ -12,14 +12,24 @@ export default function About() {
             />
 
             <div className="about__content block-content">
-                <AboutContentBox />
-                <div className="about__image">
-                    <img
-                        className="image"
-                        src={require("../../assets/images/img1.jpg").default}
-                        alt="img"
-                    />
-                </div>
+                <AboutContentBox
+                    contentSubheader="Jestem Frontend Web Developerem."
+                    contentText="Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Odit voluptatum illum sed beatae. Magni commodi dicta quia
+                    quis alias reiciendis ea unde beatae sapiente enim labore,
+                    qui sint in consectetur? Perspiciatis, odio expedita vitae
+                    perferendis sapiente magni dolore blanditiis inventore
+                    officiis eum veniam eveniet. Quod inventore eveniet
+                    voluptates voluptas. Dolore velit nisi, minima explicabo
+                    ipsum a laborum praesentium at vero! <br />
+                    <br />
+                    Earum error aut recusandae? Nobis laboriosam optio deleniti
+                    architecto veniam, quam ipsam sed ex est odit, odio maiores
+                    quas dolorum explicabo vero officia voluptate reiciendis
+                    enim a eveniet, corporis reprehenderit"
+                    imagePath="images/img1.jpg"
+                    imageDescription="img1"
+                />
             </div>
         </article>
     );
