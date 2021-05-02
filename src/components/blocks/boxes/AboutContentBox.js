2f2f3079b7d5c0a24814dbb77ae3a777607b04e0
Mathias007 <mathias.najlepszy@gmail.com>
2021-05-03 01:06:04 +0200

    rebuild AboutContentBox and modify about.scss

--- src/components/blocks/boxes/AboutContentBox.js
+++ src/components/blocks/boxes/AboutContentBox.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactHtmlParser from "react-html-parser";
 
 import Image from "../universal/Image";
 
@@ -8,28 +9,53 @@ export default function AboutContentBox(props) {
         contentText,
         imagePath,
         imageDescription,
+        id,
     } = props;
 
-    return (
-        <>
-            <div className="about__content-box">
+    if (id % 2 !== 0)
+        return (
+            <section className="about__section" key={id}>
                 <h3 className="about__subheader">
                     {contentSubheader ? contentSubheader : null}
                 </h3>
-                <p className="about__text">
-                    {contentText ? contentText : null}
-                </p>
-            </div>
-            <div className="about__image">
-                {imagePath ? (
-                    <Image
-                        imagePath={imagePath}
-                        imageDescription={
-                            imageDescription ? imageDescription : "img"
-                        }
-                    />
-                ) : null}
-            </div>
-        </>
-    );
+                <div className="about__content-box">
+                    <div className="about__text">
+                        {contentText ? ReactHtmlParser(contentText) : null}
+                    </div>
+                    <div className="about__image">
+                        {imagePath ? (
+                            <Image
+                                imagePath={imagePath}
+                                imageDescription={
+                                    imageDescription ? imageDescription : "img"
+                                }
+                            />
+                        ) : null}
+                    </div>
+                </div>
+            </section>
+        );
+    else
+        return (
+            <section className="about__section" key={id}>
+                <h3 className="about__subheader">
+                    {contentSubheader ? contentSubheader : null}
+                </h3>
+                <div className="about__content-box">
+                    <div className="about__image">
+                        {imagePath ? (
+                            <Image
+                                imagePath={imagePath}
+                                imageDescription={
+                                    imageDescription ? imageDescription : "img"
+                                }
+                            />
+                        ) : null}
+                    </div>
+                    <p className="about__text">
+                        {contentText ? ReactHtmlParser(contentText) : null}
+                    </p>
+                </div>
+            </section>
+        );
 }
