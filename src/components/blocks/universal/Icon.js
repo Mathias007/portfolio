2a8c44298451665a244106d2d3c8590050579426
Mathias007 <mathias.najlepszy@gmail.com>
2021-05-03 20:03:02 +0200

    update Icon universal component (timeline icons and props)

--- src/components/blocks/universal/Icon.js
+++ src/components/blocks/universal/Icon.js
@@ -3,15 +3,49 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import {
+    // Contact
     faMapMarker,
     faPhone,
     faEnvelope,
+    // Timeline
+    faEthernet,
+    faDragon,
+    faScroll,
+    faHatWizard,
+    faDungeon,
+    faRing,
+    faBookDead,
+    faUniversity,
+    faLaptopCode,
+    faBookOpen,
+    faGraduationCap,
+    faAward,
+    faGavel,
+    faBriefcase,
 } from "@fortawesome/free-solid-svg-icons";
 
-library.add(faMapMarker, faPhone, faEnvelope);
+library.add(
+    faMapMarker,
+    faPhone,
+    faEnvelope,
+    faEthernet,
+    faDragon,
+    faScroll,
+    faHatWizard,
+    faDungeon,
+    faRing,
+    faBookDead,
+    faUniversity,
+    faLaptopCode,
+    faBookOpen,
+    faGraduationCap,
+    faAward,
+    faGavel,
+    faBriefcase
+);
 
 export default function Icon(props) {
     const { icon } = props;
 
-    return <FontAwesomeIcon icon={icon} />;
+    return <FontAwesomeIcon icon={icon} {...props} />;
 }
