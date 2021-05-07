c56523d4a58d12a6c1f3229558440da18275c10b
Mathias007 <mathias.najlepszy@gmail.com>
2021-05-07 23:41:24 +0200

    rebuild WorkBox, update work and block scss

--- src/components/blocks/boxes/WorkBox.js
+++ src/components/blocks/boxes/WorkBox.js
@@ -1,17 +1,35 @@
 import React from "react";
 
+import Button from "../universal/Button";
 import Image from "../universal/Image";
 
 export default function WorkBox(props) {
-    const { imagePath, imageDescription } = props;
+    const {
+        projectTitle,
+        projectPreview,
+        projectCode,
+        imagePath,
+        imageDescription,
+    } = props;
 
     return (
         <section className="work-box">
-            <Image
-                imageClass="work-box__image"
-                imagePath={imagePath}
-                imageDescription={imageDescription}
-            />
+            <h3 className="work-box__title">{projectTitle}</h3>
+
+            <a href={projectPreview} className="work-box__link">
+                <Image
+                    imageClass="work-box__image"
+                    imagePath={imagePath}
+                    imageDescription={imageDescription}
+                />
+            </a>
+            {projectCode ? (
+                <Button
+                    className="button button--work"
+                    href={`https://github.com/Mathias007/${projectCode}`}
+                    text="Zobacz kod"
+                />
+            ) : null}
         </section>
     );
 }
