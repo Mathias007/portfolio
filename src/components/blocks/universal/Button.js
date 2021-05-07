7b772c8de91dbf4e74a845d544a9c87c2ad2c3b7
Mathias007 <mathias.najlepszy@gmail.com>
2021-05-07 23:42:25 +0200

    rebuild Button component (two alternatives of building).

--- src/components/blocks/universal/Button.js
+++ src/components/blocks/universal/Button.js
@@ -1,11 +1,22 @@
 import React from "react";
 
 export default function Button(props) {
-    const { className, href, text } = props;
+    const { className, href, text, onClick } = props;
 
-    return (
-        <a href={href} className={className ? className : "button"}>
-            {text}
-        </a>
-    );
+    if (onClick)
+        return (
+            <button
+                href={href}
+                className={className ? className : "button"}
+                onClick={onClick}
+            >
+                {text}
+            </button>
+        );
+    else
+        return (
+            <a href={href} className={className ? className : "button"}>
+                {text}
+            </a>
+        );
 }
