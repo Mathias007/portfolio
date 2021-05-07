bb5c5022dd5e4b127d9b66183d85df4e8d0559aa
Mathias007 <mathias.najlepszy@gmail.com>
2021-05-07 23:40:39 +0200

    extend Work block using hook and map function

--- src/components/blocks/Work.js
+++ src/components/blocks/Work.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Button from "./universal/Button";
 import BlockHeading from "./heading/BlockHeading";
+
 import WorkBox from "./boxes/WorkBox";
+import WorkListElement from "./boxes/WorkListElement";
 
 import { workData } from "../../data/workData";
 
 export default function Work() {
+    const [isActive, setActive] = useState(false);
+
+    const toggleProjectsVisibility = () => {
+        setActive(!isActive);
+    };
+
     return (
         <article className="portfolio__block work" id="work">
             <BlockHeading
@@ -19,28 +27,85 @@ export default function Work() {
             />
 
             <div className="work__content block-content">
-                <WorkBox
-                    imagePath="work/review.svg"
-                    imageDescription="review"
-                />
-                <WorkBox
-                    imagePath="work/finance.svg"
-                    imageDescription="finance"
-                />
-                <WorkBox
-                    imagePath="work/education.svg"
-                    imageDescription="education"
-                />
-                <WorkBox
-                    imagePath="work/website.svg"
-                    imageDescription="website"
-                />
+                {workData.map((element) => {
+                    const {
+                        id,
+                        projectTitle,
+                        projectPreview,
+                        projectCode,
+                        projectCategory,
+                        projectImage,
+                    } = element;
+
+                    if (projectCategory === "website")
+                        return (
+                            <WorkBox
+                                key={id}
+                                id={id}
+                                projectTitle={projectTitle}
+                                projectPreview={projectPreview}
+                                projectCode={projectCode}
+                                imagePath={projectImage}
+                                imageDescription={projectTitle}
+                            />
+                        );
+                    else return null;
+                })}
             </div>
+            {isActive ? (
+                <>
+                    <div className="work__addition block-heading">
+                        <Button
+                            className="work__button button"
+                            href="#work"
+                            text={isActive ? "Pokaż mniej" : "Pokaż więcej"}
+                            onClick={toggleProjectsVisibility}
+                        />
+                    </div>
+                    <div className="work__content block-content">
+                        {workData.map((element) => {
+                            const {
+                                id,
+                                projectTitle,
+                                projectPreview,
+                                projectCode,
+                                projectCategory,
+                            } = element;
+
+                            if (projectCategory !== "website")
+                                return (
+                                    <WorkListElement
+                                        key={id}
+                                        id={id}
+                                        projectTitle={projectTitle}
+                                        projectPreview={projectPreview}
+                                        projectCode={projectCode}
+                                        imagePath={
+                                            projectCategory === "oop-game"
+                                                ? "work/webdev2.svg"
+                                                : projectCategory ===
+                                                  "elder-js-project"
+                                                ? "work/webdev1.svg"
+                                                : projectCategory ===
+                                                  ("react-project" ||
+                                                      "big-project")
+                                                ? "work/react.svg"
+                                                : "work/javascript.svg"
+                                        }
+                                        imageDescription={projectTitle}
+                                    />
+                                );
+                            else return null;
+                        })}
+                    </div>
+                </>
+            ) : null}
             <div className="work__addition block-heading">
                 <Button
                     className="work__button button"
                     href="#work"
-                    text="Zobacz więcej"
+                    text={isActive ? "Pokaż mniej" : "Pokaż więcej"}
+                    onClick={toggleProjectsVisibility}
                 />
             </div>
         </article>
