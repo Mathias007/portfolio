77ea66f72520cd5ebfb8d1c8160973f2bda5945f
Mathias007 <mathias.najlepszy@gmail.com>
2021-05-08 20:40:43 +0200

    implement useState hook in AboutContentBox (isVisible).

--- src/components/blocks/boxes/AboutContentBox.js
+++ src/components/blocks/boxes/AboutContentBox.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactHtmlParser from "react-html-parser";
 
 import Image from "../universal/Image";
 
 export default function AboutContentBox(props) {
+    const [isVisible, setVisibility] = useState(false);
+
+    const toggleContentVisibility = () => {
+        setVisibility(!isVisible);
+    };
+
     const {
         contentSubheader,
         contentText,
@@ -15,10 +21,17 @@ export default function AboutContentBox(props) {
     if (id % 2 !== 0)
         return (
             <section className="about__section" key={id}>
-                <h3 className="about__subheader">
+                <h3
+                    className="about__subheader"
+                    onClick={toggleContentVisibility}
+                >
                     {contentSubheader ? contentSubheader : null}
                 </h3>
-                <div className="about__content-box">
+                <div
+                    className={`about__content-box ${
+                        isVisible ? "about__content-box--visible" : ""
+                    }`}
+                >
                     <div className="about__text">
                         {contentText ? ReactHtmlParser(contentText) : null}
                     </div>
@@ -38,10 +51,17 @@ export default function AboutContentBox(props) {
     else
         return (
             <section className="about__section" key={id}>
-                <h3 className="about__subheader">
+                <h3
+                    className="about__subheader"
+                    onClick={toggleContentVisibility}
+                >
                     {contentSubheader ? contentSubheader : null}
                 </h3>
-                <div className="about__content-box">
+                <div
+                    className={`about__content-box ${
+                        isVisible ? "about__content-box--visible" : ""
+                    }`}
+                >
                     <div className="about__image">
                         {imagePath ? (
                             <Image
