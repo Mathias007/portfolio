b51d187257a152a17cf92ac5fd68a6341b479355
Mathias007 <mathias.najlepszy@gmail.com>
2021-05-08 21:03:16 +0200

    build quotesData.js

---
+++ src/data/quotesData.js
@@ -0,0 +1,32 @@
+export const quotesData = [
+    {
+        id: 1,
+        quoteText:
+            "When something is important enough, you do it even if the odds are not in your favor.",
+        authorName: "Elon Musk",
+        authorTitle: "Dyrektor Generalny Tesla, Inc.",
+    },
+
+    {
+        id: 2,
+        quoteText:
+            "You can’t connect the dots looking forward; you can only connect them looking backward. So you have to trust that the dots will somehow connect in your future.",
+        authorName: "Steve Jobs",
+        authorTitle: "Założyciel Apple, Inc",
+    },
+
+    {
+        id: 3,
+        quoteText:
+            "Most good programmers do programming not because they expect to get paid or get adulation by the public, but because it is fun to program.",
+        authorName: "Linus Torvalds",
+        authorTitle: "Twórca Linux OS",
+    },
+
+    {
+        id: 4,
+        quoteText: "The only constant in the technology industry is change.",
+        authorName: "Marc Benioff",
+        authorTitle: "Dyrektor Generalny salesforce.com",
+    },
+];
