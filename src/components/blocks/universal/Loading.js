740ee329f23f56bc6b2fc6a27a5140826067dc0b
Mathias007 <mathias.najlepszy@gmail.com>
2021-05-09 00:59:20 +0200

    creat Loading component with loading.scss using as a fallback in suspense.

---
+++ src/components/blocks/universal/Loading.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Loading() {
+    return (
+        <div class="loading-spinner">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    );
+}
