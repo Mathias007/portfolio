ae42eeba86ad47b7cd2caada9d47fba91089c32d
Mathias007 <mathias.najlepszy@gmail.com>
2021-05-23 14:40:51 +0200

    make corrections of server configuration for production aims (heroku).

--- server/src/index.js
+++ server/src/index.js
@@ -12,12 +12,12 @@ import { paths } from "./config/names";
 import config from "./config/config";
 
 const { root } = paths;
-const { NODE_PORT, MONGO_URI, CORS_ORIGIN } = config;
+const { NODE_PORT, MONGO_URI } = config;
 
 const app = express();
 
 const corsOptions = {
-    origin: CORS_ORIGIN,
+    origin: "https://mathias.net.pl",
 };
 app.use(cors(corsOptions));
 
@@ -30,6 +30,7 @@ mongoose
     .catch((err) => console.log(DATABASE_CONNECTION_ERROR_MESSAGE(err)));
 
 app.use(root, routes);
-app.listen(NODE_PORT, () => {
-    console.log(PORT_LISTENING_START_MESSAGE(NODE_PORT));
+
+app.listen(process.env.PORT || 5000, "0.0.0.0", () => {
+    console.log(PORT_LISTENING_START_MESSAGE(NODE_PORT || 5000));
 });
